38033a3cc127bc826531f206ab2b9212811323d3
Victor <papadoginc@gmail.com>
2016-11-08 19:14:14 +0300

    Initialize repo

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+    requiredAuthentication: function(req, res, next){
+        console.log('private route hit');
+        next();
+    },
+    logger: function(req, res, next){
+        console.log("Reguest: " + new Date().toDateString()+" "+req.method +" "+ req.originalUrl);
+        next();
+    }
+};
+
+//app.use(middleware.requiredAuthentication);
+app.use(middleware.logger);
+
+app.get('/about',middleware.requiredAuthentication,function(req, res){
+    res.send("About us");
+});
+
+app.use(express.static(__dirname+'/public'));
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server is running on port "+PORT);
+});
